621b1b77f2d2dbd08fbd3e6399a5909939f76285
Aman <amanmuzaffarpur11@gmail.com>
2021-06-26 21:53:46 +0530

    first commit
    you can easily calculate your body mas index by this program

---
+++ BMI.py
@@ -0,0 +1,14 @@
+print("________Welcome to Body-Mass Index calculator______________")
+print("-----------------------------------------------------------")
+
+height=float(input("enter your height in metres : "))
+weight=float(input("enter your weight in kilograms :"))
+bmi=(weight/(height*height))
+print("Your BMI is :", +bmi)
+if(bmi<18.5):
+    print("nyou are under-weight, you should get in touch with a doctor")
+elif(bmi>18.5 and bmi<24.9):
+    print("you are normal, keep it regular ALL THE BEST")
+else:
+    print("you are over-weight, control your diet and consult a doctor")
+
