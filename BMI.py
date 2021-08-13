43f2cf9f648cd0c12c7cc820ae71b19cdf032c96
Aman <amanmuzaffarpur11@gmail.com>
2021-08-13 19:55:17 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -8,7 +8,7 @@ print("Your BMI is :", +bmi)
 if(bmi<18.5):
     print("nyou are under-weight, you should get in touch with a doctor")
 elif(bmi>18.5 and bmi<24.9):
-    print("you are normal, keep it regular ALL THE BEST")
+    print("You are normal, keep it regular ALL THE BEST")
 else:
-    print("you are over-weight, control your diet and consult a doctor")
+    print("You are over-weight, control your diet and consult a doctor")
 
